b905d1d2cf6ceddf2fe15933c170023c2632479b
Samantha Maturen & Jordan Johansen <student@epicodus.com>
2015-08-27 11:31:25 -0700

    Create Patron class.

--- src/Patron.php
+++ src/Patron.php
@@ -1,45 +1,117 @@
 <?php
-    /* The Patron class should track a library patron's info and be able to join the checkouts table*/
-    //UNFINISHED!
-    Class Patron
+
+    class Patron
     {
-        private $name;
-        private $phone_number;
+        private $patron_name;
         private $id;
 
-        function __construct($name, $phone_number, $id = null);
+        function __construct($patron_name, $id = null)
         {
-            $this->name = $name;
-            $this->phone_number = $phone_number;
-            $id->id = $id;
+            $this->patron_name = $patron_name;
+            $this->id = $id;
         }
 
-        function setName($new_name)
+        function setPatronName($new_patron_name)
         {
-            $this->name = $$new_name;
+            $this->patron_name = $new_patron_name;
         }
 
-        function getName()
+        function getPatronName()
         {
-            return $this->name;
-        }
-
-        function setPhoneNumber($new_phone_number)
-        {
-            $this->phone_number = $new_phone_number;
-        }
-
-        function getPhoneNumber()
-        {
-            return $this->phone_number;
+            return $this->patron_name;
         }
 
         function getId()
         {
             return $this->id;
         }
+
+        function save()
+        {
+            $GLOBALS['DB']->exec("INSERT INTO patrons (patron_name)
+            VALUES ('{$this->getPatronName()}');");
+            $this->id = $GLOBALS['DB']->lastInsertId();
+        }
+
+        static function getAll()
+        {
+            $returned_patrons = $GLOBALS['DB']->query("SELECT * FROM patrons;");
+            $patrons = array();
+            foreach($returned_patrons as $patron) {
+                $patron_name = $patron['patron_name'];
+                $id = $patron['id'];
+                $new_patron = new Patron($patron_name, $id);
+                array_push($patrons, $new_patron);
+            }
+            return $patrons;
+        }
+
+        function delete()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM patrons WHERE id = {$this->getId()};");
+            // to delete patron from potential join table
+            // $GLOBALS['DB']->exec("DELETE FROM authors_books
+            // WHERE patron_id = {$this->getId()};");
+        }
+
+        static function deleteAll()
+        {
+          $GLOBALS['DB']->exec("DELETE FROM patrons;");
+        }
+
+        static function find($search_id)
+        {
+            $found_patron = null;
+            $patrons = Patron::getAll();
+            foreach($patrons as $patron) {
+                $patron_id = $patron->getId();
+                if ($patron_id == $search_id) {
+                    $found_patron = $patron;
+                }
+            }
+            return $found_patron;
+        }
+
+        static function searchPatron($search_patron)
+        {
+            $found_patrons = array();
+            $results = $GLOBALS['DB']->query("SELECT * FROM patrons
+            WHERE patron_name = '{$search_patron}';");
+
+            foreach($results as $patron) {
+                $patron_name = $patron['patron_name'];
+                $id = $patron['id'];
+                $found_patron = new Patron($patron_name, $id);
+                array_push($found_patrons, $found_patron);
+            }
+            return $found_patrons;
+        }
+
+        // Potential getCopies from join table (checkouts)
+        // function getAuthors()
+        // {
+        //     $authors = array();
+        //     $results = $GLOBALS['DB']->query("SELECT authors.* FROM
+        //         books JOIN authors_books ON (books.id = authors_books.book_id)
+        //               JOIN authors ON (authors_books.author_id = authors.id)
+        //               WHERE books.id = {$this->getId()};");
+        //
+        //     foreach($results as $author) {
+        //         $author_name = $author['author_name'];
+        //         $id = $author['id'];
+        //         $new_author = new Author($author_name, $id);
+        //         array_push($authors, $new_author);
+        //     }
+        //     return $authors;
+        // }
+
+        function update($new_patron_name)
+        {
+            $GLOBALS['DB']->exec("UPDATE patrons SET patron_name = '{$new_patron_name}'
+                WHERE id = {$this->getId()};");
+            $this->setPatronName($new_patron_name);
+        }
+
     }
-
-
 
 ?>
